fae300d3f6492d20a82789378cf7ffa4efbf67ec
Jesus24-Dev <siritjesus24@gmail.com>
2026-04-21 12:25:08 -0400

    feat:  Middleware de validacion agregado utilizando ZOd para validacion de esquemas.

---
+++ backend/src/shared/validate.ts
@@ -0,0 +1,12 @@
+import type { Request, Response, NextFunction } from 'express';
+import { ZodObject } from 'zod';
+
+export const validate = (schema: ZodObject) => 
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse({ body: req.body, query: req.query, params: req.params });
+      next();
+    } catch (e: any) {
+      return res.status(400).json(e.errors);
+    }
+  };

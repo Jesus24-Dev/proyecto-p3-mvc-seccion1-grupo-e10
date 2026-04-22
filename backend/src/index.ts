df37c68857b418774799931c731a06a9e106273c
Jesus24-Dev <siritjesus24@gmail.com>
2026-04-22 15:28:19 -0400

    feat: Refactorizacion del archivo index.ts para usar UserRoutes como manejo de rutas

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
-import prisma from './database/prisma.js';
+import {UserRoutes} from "./features/Users/index.js";
 
 dotenv.config();
 
@@ -12,11 +12,7 @@ const PORT = process.env.PORT || 3000
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-
-app.get("/", async (req, res) => {
-    const users = await prisma.users.findMany();
-    res.json("API works correctly!");
-});
+app.use('/users', UserRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
